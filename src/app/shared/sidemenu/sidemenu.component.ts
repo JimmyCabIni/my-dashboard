e1a08ee1495a13d2c2266b5ea1ef6e5cb7d1ec43
Jaime <jaimecaballero99@gmail.com>
2024-04-23 14:09:11 +0200

    Creación de Alias

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,7 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
+
 
 @Component({
   selector: 'app-sidemenu',
