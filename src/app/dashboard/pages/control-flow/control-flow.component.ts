22ea2c2e79a0d86ea6d639a59130cde0d8f56585
Jaime <jaimecaballero99@gmail.com>
2024-04-23 14:28:50 +0200

    Control Flow -  @switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A'|'B'|'F';
+
 @Component({
   standalone: true,
   imports: [CommonModule],
@@ -10,9 +12,11 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('F');
 
   public toggleContent() {
     this.showContent.update( value => !value )
+
   }
 
 }
